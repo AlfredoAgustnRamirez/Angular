3f4bbf3303ede2776976d53312ba2dbe1ebf39de
Agustinn35 <ramirezalfredoagustin80@mail.com>
2024-05-22 17:03:40 -0300

    auth

--- src/app/core/auth/components/auth.component.ts
+++ src/app/core/auth/components/auth.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-auth',
@@ -8,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './auth.component.css'
 })
 export class AuthComponent {
+  passwordVisible = false
+  email: string = ''
+  password: string = ''
+  userId: string  = ''
+  nombreUsr: string = ''
+  router: any;
 
+  constructor(
+    private AuthService: AuthService,
+  ){ }
+
+  login(){
+    const payload = {
+      email: this.email,
+      password: this.password
+    } 
+    
+    this.AuthService.login(payload).subscribe((session => {
+       console.log('Respuesta del servicio:', session);
+     if (session) {
+         localStorage.setItem('SESSION.localStorage', JSON.stringify(session));
+         this.userId = session.userId;
+         this.nombreUsr = session.nombreUsr;
+         const userId = session.userId;
+         localStorage.setItem('userId', userId);
+         this.router.navigate(['/welcome']);
+       } 
+     }),
+   );
+  }
 }
